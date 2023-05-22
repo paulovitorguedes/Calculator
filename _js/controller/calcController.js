4b3904ad1602def7f13f38f11840c392e1704a3a
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2023-05-21 23:23:43 -0300

    class Calculator
    Inicialização da classe Calculator

--- _js/controller/calcController.js
+++ _js/controller/calcController.js
@@ -1,10 +1,17 @@
 class Calculator {
 
     construdtor() {
-
+        this._displayEl = document.querySelector('visor');
     }
 
     initialise() {
-        
+
+    }
+
+    get display() {
+        return this._displayEl;
+    }
+    set display(value) {
+        this._displayEl = value;
     }
 }
