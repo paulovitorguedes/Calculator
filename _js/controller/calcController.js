3d810fe1a4779e90ea50f1d600352184e763d1e9
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2023-06-16 17:45:18 -0300

    function initinitBtnEvents
    A função adiciona os eventos do mouse 'click e drag' dos botôes da calculadora

--- _js/controller/calcController.js
+++ _js/controller/calcController.js
@@ -1,18 +1,47 @@
 class Calculator {
 
     constructor() {
-        // this._displayEl = document.querySelector('#visor');
-        this.initEventListener();
+        this._operation = [0];
+        this._displayEl = document.querySelector('#display');
+
+        this.initialise();
     }
 
     initialise() {
+        this.initBtnEvents();
 
+        this._displayEl.value = this._operation[0];
     }
 
-    initEventListener() {
-        let btn = document.querySelectorAll('.btn');
-        console.log(btn);
+
+    // A função deve receber os elementos html, os eventos separados por (" ") e a função 
+    addEventListenerAll(element, event, fn) {
+        event.split(" ").forEach(e => {
+            element.addEventListener(e.trim(), fn)
+        })
     }
+
+    //Adiciona os eventos 'click drag' aos btn chamando a função execBtn
+    initBtnEvents() {
+        let elements = document.querySelectorAll('.btn');
+        elements.forEach(element => {
+            this.addEventListenerAll(element, 'click drag', e => {
+                this.execBtn(element.innerHTML);
+            })
+        })
+    }
+
+    
+
+
+
+
+    execBtn(value) {
+        console.log(value);
+    }
+
+
+
 
     get display() {
         return this._displayEl;
